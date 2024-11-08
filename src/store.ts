9b0a1869337e451cd83b172ed84212dd45e7cad3
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-11-07 20:25:48 -0500

    add Axios and fetchCryptoAPI

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,18 @@
 import { create } from 'zustand';
+import axios from 'axios';
+import { CryptoCurrenciesResponseSchema } from './schema/crypto-schema';
 
-
+async function getCryptos() {
+    const url = `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD`
+    const {data: {Data}} = await axios.get(url)
+    
+    const result = CryptoCurrenciesResponseSchema.safeParse(Data)
+    console.log(result)
+}
 
 
 export const useCryptoStore = create(() => ({
     fetchCryptos: () => {
-        console.log('holaa')
+        getCryptos()
     }
 }))
